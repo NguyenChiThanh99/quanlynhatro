2350b1d8f26e8d1a4efca35a0366e703add4ea81
NguyenChiThanh99 <nguyenchithanh1999@gmail.com>
2022-04-12 18:00:35 +0700

    Change server link

--- src/components/Global.js
+++ src/components/Global.js
@@ -1,17 +1,32 @@
 module.exports = {
-  server: "https://backend-quanlynhatro-online.herokuapp.com/",
+  // server: "https://backend-quanlynhatro-online.herokuapp.com/",
+  server: "  https://quanlynhatro-backend.herokuapp.com/",
+
   formatDate: (date) => {
     var d = new Date(date);
     var datestring =
-    ("0" + d.getDate()).slice(-2) + "/" + ("0" + (d.getMonth() + 1)).slice(-2) + "/" + d.getFullYear();
+      ("0" + d.getDate()).slice(-2) +
+      "/" +
+      ("0" + (d.getMonth() + 1)).slice(-2) +
+      "/" +
+      d.getFullYear();
     return datestring;
   },
 
   formatFullDate: (date) => {
     var d = new Date(date);
     var datestring =
-    ("0" + d.getDate()).slice(-2) + "/" + ("0" + (d.getMonth() + 1)).slice(-2) + "/" + d.getFullYear();
-    var timestring = ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2) + ":" + ("0" + d.getSeconds()).slice(-2)
+      ("0" + d.getDate()).slice(-2) +
+      "/" +
+      ("0" + (d.getMonth() + 1)).slice(-2) +
+      "/" +
+      d.getFullYear();
+    var timestring =
+      ("0" + d.getHours()).slice(-2) +
+      ":" +
+      ("0" + d.getMinutes()).slice(-2) +
+      ":" +
+      ("0" + d.getSeconds()).slice(-2);
     return timestring + " " + datestring;
   },
 
